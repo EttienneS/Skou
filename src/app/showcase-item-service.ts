9451f1038bed7fb6074eb08a25def607ff277926
Ettienne Scharneck <EttienneS@k2.com>
2020-10-20 13:22:41 +0200

    update styling

--- src/app/showcase-item-service.ts
+++ src/app/showcase-item-service.ts
@@ -10,7 +10,7 @@ export class ShowcaseItemService {
     {
       Name: 'Lekker Skou',
       SubTitle: 'Kom kyk wat ons maak!',
-      Description: `A "Lekker" web application showcase to have a handy landing page for all other projects.`,
+      Description: `A "Lekker" web application showcase to have a handy landing page for all other projects`,
       Image:
         'https://images.unsplash.com/photo-1596778402284-8398c7b09521?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       ProjectUrl: 'https://github.com/lekker-dev/Lekker.Skou',
@@ -20,11 +20,7 @@ export class ShowcaseItemService {
     {
       Name: 'Lekker Kort',
       SubTitle: 'Maak hom lekker kort!',
-      Description: `Tired of drab, informative URLs? Why not inject some 'lekker' flavour into your links with Lekker.Kort!
-      
-      Using our patented[1] lekkerification algorithm[2] you too can give have lekker URL!
-
-      [1] not even slightly patented [2] very simple number-text lookup`,
+      Description: `Tired of drab, informative URLs? Why not inject some 'lekker' flavour into your links with Lekker.Kort!`,
       Image:
         'https://lekkerkort.azurewebsites.net/hu-chen-60XLoOgwkfA-unsplash.0c26fed9ae688c2213ed.jpg',
       ProjectUrl: 'https://github.com/lekker-dev/Lekker.Kort',
@@ -34,7 +30,7 @@ export class ShowcaseItemService {
     {
       Name: 'Lekker Luister',
       SubTitle: 'Luister na iets lekker!',
-      Description: `A "Lekker" web application to get some music information from music streaming services such as Deezer, Spotify etc. through their open API's and implementing it with Angular and .net Core.`,
+      Description: `A "Lekker" web application to get some music information from music streaming services such as Deezer and Spotify`,
       Image:
         'https://images.unsplash.com/photo-1445985543470-41fba5c3144a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
       ProjectUrl: '',
